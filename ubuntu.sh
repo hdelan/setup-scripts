06d6091638d23782fb6056260d857cee391a472e
Hugh Delaney <hugh.delaney@codeplay.com>
2022-08-16 12:05:24 +0100

    First file

---
+++ ubuntu.sh
@@ -0,0 +1,30 @@
+if [ "$EUID" -ne 0 ]
+  then echo "This setup script must be run as root."
+  exit
+fi
+
+set -e
+set -v
+
+cd
+HOME=$(pwd)
+
+apt update && apt upgrade -y
+apt install git zsh build-essential nodejs curl wget neovim cmake ninja-build -y
+
+sudo snap install node --classic
+
+# Neovim stuff
+python3 -m pip install pynvim
+
+# Vim-plug for Neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cd .config && git clone https://github.com/hdelan/nvim.git
+
+# CUDA toolkit 
+wget https://developer.download.nvidia.com/compute/cuda/11.7.1/local_installers/cuda_11.7.1_515.65.01_linux.run
+sh cuda_11.7.1_515.65.01_linux.run
+
+# Ohmyzsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
